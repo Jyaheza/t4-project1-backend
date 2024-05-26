d5b89151b2af5640427484e99e785b0fab64efab
Mario Mendiola Jr <mariomendiolajr@Marios-MBP-2.attlocal.net>
2024-05-25 21:56:44 -0500

    reformat session

--- app/models/session.model.js
+++ app/models/session.model.js
@@ -1,10 +1,14 @@
-const Session = sequelize.define("session", {
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    expirationDate: {
-      type: Sequelize.DATE,
-      allowNull: false,
-    },
-  });
+module.exports = (sequelize, Sequelize) => {
+    const Session = sequelize.define("session", {
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        expirationDate: {
+            type: Sequelize.DATE,
+            allowNull: false,
+        },
+    });
+
+    return Session;
+};
