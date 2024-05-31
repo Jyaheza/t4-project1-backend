56bf5d20e02b610ae4855d55e7ce147469fb8f49
Mario Mendiola Jr <mariomendiolajr@Marios-MBP-2.attlocal.net>
2024-05-31 01:39:06 -0500

    Rebase

--- app/routes/stories.routes.js
+++ app/routes/stories.routes.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-  module.exports = (app) => {
-    const Story = require("../controllers/stories.controller.js");
-    const { authenticateRoute } = require("../authentication/authentication");
-    var router = require("express").Router();
-  
-    // Create a new Story
-    router.post("/stories/:userId", Story.create);
-
-    router.post("/stories/extend/:storyId", Story.extend);
-
-    router.get(`/stories/:userId`, Story.findAllParentStoriesForUser);
-  
-    app.use("/storiesapi", router);
-  };
-  
-=======
 module.exports = (app) => {
   const Story = require("../controllers/stories.controller.js");
   const { authenticateRoute } = require("../authentication/authentication");
